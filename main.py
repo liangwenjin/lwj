56bf60db55e62b33ad99343598241dc0c5628e2d
liangwenjin <51104319+liangwenjin@users.noreply.github.com>
2019-05-28 11:37:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-天共白云晓
+import ujson
+import time
+import MQTT
+import machine
+
+print('**************adding from pythonA')
+print('**************this is a demo *******')
+print('**************master adding *****8888*')
